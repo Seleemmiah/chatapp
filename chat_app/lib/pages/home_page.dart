1d51e4d835ea9331a3bcff69bfce3b11c7964c42
“Seleemmiah” <seleemmiah1@gmail.com>
2025-07-09 01:28:35 +0100

    configured firestore database

--- chat_app/lib/pages/home_page.dart
+++ chat_app/lib/pages/home_page.dart
@@ -1,25 +1,21 @@
-import 'package:chat_app/auth/auth_service.dart';
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void logout() {
-    // get auth service
-    final _auth = AuthService();
-    _auth.signout();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
         title: Text('Home Page'),
-        actions: [
-          // logout button
-          IconButton(onPressed: logout, icon: Icon(Icons.logout)),
-        ],
+        
       ),
+      drawer: MyDrawer(),
     );
   }
 }
---
+++ chat_app/lib/services/chat/chat_service.dart
@@ -0,0 +1,11 @@
+class ChatService {
+  // get instance of the firestore
+
+  // get user stream
+
+  // send message
+
+  // get messages stream
+
+
+}
