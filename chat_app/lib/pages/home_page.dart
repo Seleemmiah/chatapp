7adedcbe23a2968424a483158a3809fd6adc7d7e
“Seleemmiah” <seleemmiah1@gmail.com>
2025-07-12 19:41:24 +0100

    resolved the issue in the code and push the update

--- chat_app/lib/pages/home_page.dart
+++ chat_app/lib/pages/home_page.dart
@@ -1,9 +1,14 @@
-import 'package:chat_app/services/auth/auth_service.dart';
 import 'package:chat_app/components/my_drawer.dart';
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+   HomePage({super.key});
+
+  // chat and auth service
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
 
   @override
--- chat_app/lib/services/chat/chat_service.dart
+++ chat_app/lib/services/chat/chat_service.dart
@@ -1,11 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatService {
   // get instance of the firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Stream<List<Map<String, dynamic>>> getUserStream() {
+    // return a stream of user data
+    return _firestore.collection('users').snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        // go through each individual user
+        final user = doc.data();
+
+        return user;
+      }).toList();
+    });
+  }
 
   // get user stream
 
   // send message
 
   // get messages stream
-
-
 }
